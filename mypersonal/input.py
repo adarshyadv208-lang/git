adf0b2eb4e37dc315aeb7478972e8750f59686a8
adarshyadv208-lang <adarshyadv208@gmail.com>
2026-09-26 03:59:45 +0545

    Example of real difference between input and int(input)

---
+++ mypersonal/input.py
@@ -0,0 +1,13 @@
+#Differnce between input and int(input)
+#input example
+a = input("Enter a fist number - ")
+b = input("Enter a second number - ")
+sum = a + b # 5 + 3 = 53
+
+print (sum)
+
+#int(input) example
+a = int(input("Enter a fist number-"))
+b = int(input("Enter a second number-"))
+sum = a + b 
+print (sum) # 5+3 = 8
